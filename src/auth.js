1f2e949aaef5f5369a5339f3d7f76300c0bc3209
Shilpa <z5421391@ad.unsw.edu.au>
2024-06-05 19:07:47 +1000

    iteration0/Shilpa/adminAuthLogin

---
+++ src/auth.js
@@ -0,0 +1,13 @@
+/**
+ * Validates a user's login, given their email and password.
+ * 
+ * @param {number} authUserId - unique identifier for a user
+ * @param {string} email - user's email
+ * @param {string} password - user's matching password
+ * @returns {authUserId}
+ */
+function adminAuthLogin(email, password) {
+
+
+    return {authUserId: 1};
+}
