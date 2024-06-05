59ed22f2f4395bd4c61ad95be9820f35f7b6a0f1
Krish Kadamb Patel <z5508882@ad.unsw.edu.au>
2024-06-05 16:29:17 +1000

    Added stub for adminQuizRemove

--- src/quiz.js
+++ src/quiz.js
@@ -1,12 +1,18 @@
+
 /**
- * This function permanently removes the quiz,
- * when it is given the quiz as the input 
+ * This function gets all of the relevant information,
+ * about the current quiz 
  *
  * @param {number} authUserId - ID of the authorized user
  * @param {number} quizId - ID of the quiz
  * @returns {object}
  */
-function adminQuizRemove(authUserId, quizId) {
-    return {};
+function adminQuizInfo(authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+      };
 }
-
