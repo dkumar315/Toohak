57918b315616254b0f943151ff457286249d422c
Devaansh Kumar <z5474396@ad.unsw.edu.au>
2024-06-05 23:36:39 +1000

    Added stub code for the function adminQuizDescriptionUpdate

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+/**
+ * This function updates the description of the relevant quiz.
+ * 
+ * @param {number} authUserId - ID of the authorised user
+ * @param {number} quizId - ID of the quiz
+ * @param {string} description - Description of the user
+ * @returns {object} - Returns an empty object
+ */
+function adminQuizDescriptionUpdate(authUserId, quizId, description) {
+    return {};
+}
