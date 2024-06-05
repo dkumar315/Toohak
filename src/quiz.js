32cf0c2a10eea9aa7aeb2792eada7b2052ba9a3c
Devaansh Kumar <z5474396@ad.unsw.edu.au>
2024-06-05 23:43:32 +1000

    Added stub code for the function adminQuizNameUpdate

--- src/quiz.js
+++ src/quiz.js
@@ -3,7 +3,7 @@
  * 
  * @param {number} authUserId - ID of the authorised user
  * @param {number} quizId - ID of the quiz
- * @param {String} name - Name of the user
+ * @param {string} name - Name of the user
  * @returns {object} - Returns an empty object
  */
 function adminQuizNameUpdate(authUserId, quizId, name) {
