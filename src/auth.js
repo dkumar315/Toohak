3f26bd86011b43924a3250d8517d5eff4246aeb6
Shilpa <z5421391@ad.unsw.edu.au>
2024-06-05 20:20:09 +1000

    fix comment to adminAuthLogin stub

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,6 @@
 /**
  * Validates a user's login, given their email and password.
  * 
- * @param {number} authUserId - unique identifier for a user
  * @param {string} email - user's email
  * @param {string} password - user's matching password
  * @returns {authUserId}
