5398eb7258ae0e9881346ff75a0cac06bd705744
Shilpa <z5421391@ad.unsw.edu.au>
2024-06-13 22:19:00 +1000

    implement test cases for authAdminLogin function from auth.js

---
+++ src/auth.test.js
@@ -0,0 +1,31 @@
+
+describe('adminAuthRegister', () => {
+    
+    describe('when one email is created', () => {
+        test.todo('Test for email being used by another user');
+        
+
+        test.todo('Email does not satisfy string only');
+            // var validator = require('validator');
+            // validator.isEmail('foo@bar.com');
+    });
+
+    describe('when one username is created', () => {
+        // NameFirst contains characters other than lowercase letters, uppercase letters, spaces, hyphens, or apostrophes.
+        test.todo('test for nameFirst input');
+            // write one test case containing lowercase, uppercase
+            // spaces, hyphens or apostrophes.
+            // has less than 2 characters, more than 20 characters
+        
+        test.todo('test for nameLast input');
+        // same as nameFirst test case.
+    });
+
+
+    test.todo('test for password input');
+    // password is less than 8 characters
+    // does not contain at least one number and at least one letter.
+
+    test.todo('Test for addSong return value (userID), behaviour and side effects');
+
+});
