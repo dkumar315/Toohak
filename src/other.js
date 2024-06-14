0b0aeabb9e65fd7b82b0b5b36bf68bf4cdc5cd19
Yuhua Zheng <z5411307@ad.unsw.edu.au>
2024-06-15 01:27:44 +1000

    new branch merged, git checkout master -- src/other.js src/dataStore.js to avoid conflicts

--- src/other.js
+++ src/other.js
@@ -1,8 +1,15 @@
+import {
+  setData,
+} from './dataStore.js';
+
 /**
  * Reset the state of the application back to the start.
  *
  * @return {object} empty object
  */
-function clear() {
+export function clear() {
+  data = {};
+  setData(data);
+  
   return {};
 }
