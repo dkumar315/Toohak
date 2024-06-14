f19af2f5fa34ec08bd7a68b8c75f82905bb83d61
Yuhua Zheng <z5411307@ad.unsw.edu.au>
2024-06-15 01:32:01 +1000

    auth.js fixed helper functions comment

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,5 @@
 import { getData, setData } from './dataStore';
+import isEmail from 'validator/lib/isEmail';
 
 /**
  * Register a user with an email, password, and names.
@@ -43,9 +44,10 @@ export function adminUserDetails(authUserId) {
 
   if (userIndex === -1) return {error:'invalid authUserId'};
   const userDetails = data.users[userIndex];
+  
   // assigning it to resolve over long line
-  const {userId, nameFirst, nameLast, email} = userDetails;
-  const {numSuccessfulLogins, numFailedPasswordsSinceLastLogin} = userDetails;
+  const {userId, nameFirst, nameLast, email, 
+  numSuccessfulLogins, numFailedPasswordsSinceLastLogin} = userDetails;
 
   return {
     user: {
@@ -87,16 +89,75 @@ function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
 }
 
 /**
+ * Given an admin user's authUserId, return its corresponding userIndex
+ * 
+ * @param {number} authUserId - unique identifier for a user
+ * 
+ * @return {object} return corresonding index of data.users
+ */
+function isValidUser(authUserId) {
+  const data = getData();
+
+  if (!authUserId) return -1;
+
+  return data.users.findIndex((array) => array.userId === authUserId);
+}
+
+/**
+ * Given an email, return true if it is not used by the other and it is email
+ * 
+ * @param {number} authUserId - unique identifier for a user, 
+ * set to -1 if it is new user
+ * @param {strung} email - user's email, according to 
+ * https://www.npmjs.com/package/validator
+ * 
+ * @return {object} return true if email is valid and not used by others
+ */
+function isValidEmail(email, authUserId) {
+  const data = getData();
+  let isUsed = true;
+  const isRegistered = data.users.filter((user) => user.email === email);
+
+  if (isRegistered.length === 0 || 
+    (isRegistered.length === 1 && isRegistered[0].userId === authUserId)) {
+    isUsed = false;
+  }
+
+  return !isUsed && isEmail(email);
+}
+
+/**
  * Given a name string, return true iif contains [a-z], [A-Z], " ", "-", or "'"
  * 
  * @param {string} name - nameFirst or nameLast of a user 
  * 
  * @return {boolean} true iif contains lettes, spaces, hyphens, or apostrophes
  */
-function isValidname(name) {
-  const reg = new RegExp(/[^a-zA-Z\s-\']/);
+function isValidName(name) {
+  const pattern = new RegExp(/[^a-zA-Z\s-\']/);
 
-  if (name.length <= 2 || name.length >= 20 || reg.test(name)) return false;
+  if (name.length < 2 || name.length > 20 || pattern.test(name)) return false;
+
+  return true;
+}
+
+/**
+ * Given a password string, return false if its length is smaller than 8, or
+ * not contain at least a letter and at least a number, otherwise return true
+ * potential upgrade: return the strength of password, return -1 if invalid
+ * 
+ * @param {string} password - nameFirst or nameLast of a user 
+ * 
+ * @return {boolean} true iif len >8 && contains at least one lette and integer
+ */
+function isValidPassword(password) {
+  const stringPattern = new RegExp(/[a-zA-Z]/);
+  const numberPattern = new RegExp(/[0-9]/);
+
+  if (password.length < 8 || !stringPattern.test(password) || 
+    !numberPattern.test(password)) {
+    return false;
+  }
 
   return true;
 }
