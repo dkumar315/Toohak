e1de4fa3775153238eca7ecbeeda0b972d313d6c
Devaansh Kumar <z5474396@ad.unsw.edu.au>
2024-06-18 01:36:35 +1000

    Edited all test cases for an incorrect bracket and updated the name already in use test with a new quiz

--- src/adminQuiz.test.js
+++ src/adminQuiz.test.js
@@ -10,7 +10,7 @@ import {
 import {
   adminQuizCreate,
   adminQuizInfo,
-  adminQuizNameUpdate
+  adminQuizNameUpdate,
 } from './quiz.js';
 
 describe('Testing for adminQuizNameUpdate', () => {
@@ -20,6 +20,8 @@ describe('Testing for adminQuizNameUpdate', () => {
   let userId2;
   let quizId2;
   let quizInfo2;
+  let quizId3;
+  let quizInfo3;
 
   beforeEach(() => {
     clear();
@@ -33,47 +35,52 @@ describe('Testing for adminQuizNameUpdate', () => {
     adminAuthLogin('krishp@gmail.com', 'KrishP02');
     quizId2 = adminQuizCreate(userId2.authUserId, 'Your Quiz', 'Quiz on Implementation');
     quizInfo2 = adminQuizInfo(userId2.authUserId, quizId2.quizId);
+
+    adminAuthRegister('devk@gmail.com', 'DevK01', 'Devaansh', 'Kumar');
+    adminAuthLogin('devk@gmail.com', 'DevK01');
+    quizId3 = adminQuizCreate(userId1.authUserId, 'Our Quiz', 'Quiz on Ethics');
+    quizInfo3 = adminQuizInfo(userId1.authUserId, quizId3.quizId);
   });
 
   test('Valid authUserId, quizId and name', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, 'Other quiz').toStrictEqual({}));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, 'Other quiz')).toStrictEqual({});
     quizInfo1 = adminQuizInfo(userId1.authUserId, quizId1.quizId);
-    expect((quizInfo1.name).toStrictEqual('Other quiz'));
+    expect((quizInfo1.name)).toStrictEqual('Other quiz');
   });
   
   test('Invalid authUserId', () => {
-    expect(adminQuizNameUpdate(3, quizInfo1.quizId, 'Other quiz').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(3, quizInfo1.quizId, 'Other quiz')).toStrictEqual({ error: expect.any(String) });
   });
   
   test('QuizId does not refer to a valid quiz', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, 3, 'Other quiz').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, 3, 'Other quiz')).toStrictEqual({ error: expect.any(String) });
   });
 
   test('QuizId does not refer to a quiz that this user owns', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo2.quizId, 'Other quiz').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo2.quizId, 'Other quiz')).toStrictEqual({ error: expect.any(String) });
   });
 
   test('Name with symbols and alphanumeric characters', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, '!M@y# $Q%u^i&z*()').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, '!M@y# $Q%u^i&z*()')).toStrictEqual({ error: expect.any(String) });
   });
 
   test('Name with only symbols', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, '!@#$%^&*()').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, '!@#$%^&*()')).toStrictEqual({ error: expect.any(String) });
   });
 
   test('Empty name', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, '').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, '')).toStrictEqual({ error: expect.any(String) });
   });
 
   test('Name less than 3 characters', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, 'No').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, 'No')).toStrictEqual({ error: expect.any(String) });
   });
 
   test('Name more than 30 characters', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, 'This name is longer than 30 characters').toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, 'This name is longer than 30 characters')).toStrictEqual({ error: expect.any(String) });
   });
 
   test('Name already in use', () => {
-    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, quizInfo2.name).toStrictEqual({ error: expect.any(String) }));
+    expect(adminQuizNameUpdate(userId1.authUserId, quizInfo1.quizId, quizInfo3.name)).toStrictEqual({ error: expect.any(String) });
   });
 });
