f1983fd8fe6cf2b1aa87cae03f3232c06467da72
Devaansh Kumar <z5474396@ad.unsw.edu.au>
2024-06-17 21:13:22 +1000

    Added implementation of adminQuizNameUpdate with error handling

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,8 @@
+import {
+  getData,
+  setData,
+} from './dataStore';
+
 /**
  * This function provides a list of all quizzes that 
  * are owned by the currently logged in user.
@@ -74,7 +79,45 @@ function adminQuizInfo(authUserId, quizId) {
  * 
  * @return {object} - Returns an empty object
  */
-function adminQuizNameUpdate(authUserId, quizId, name) {
+export function adminQuizNameUpdate(authUserId, quizId, name) {
+  const data = getData();
+  
+  const user = data.users.find(user => user.authUserId === authUserId);
+  if (!user) {
+    return { error: 'AuthUserId is not a valid user' };
+  }
+  
+  const quiz = data.quizzes.find(quiz => quiz.quizId === quizId);
+  if (!quiz) {
+    return { error: 'Quiz ID does not refer to a valid quiz' };
+  }
+  
+  if (quiz.creatorId !== authUserId) {
+    return { error: 'Quiz ID does not refer to a quiz that this user owns' };
+  }
+  
+  for (const letter of name) {
+    if (!((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z') || (letter >= '0' && letter <= '9'))) {
+      return { error: 'Name contains invalid characters, valid characters are alphanumeric and spaces' };
+    }
+  }
+  
+  if (name.length < 3) {
+    return { error: 'Name is less than 3 characters long' };
+  }
+
+  if (name.length > 30) {
+    return { error: 'Name is more than 30 characters long.' };
+  }
+  
+  const nameInUse = data.quizzes.find(q => q.creatorId === authUserId && q.quizId !== quizId && q.name === name);
+  if (nameInUse) {
+    return { error: 'Name is already used by the current logged in user for another quiz' };
+  }
+  
+  quiz.name = name;
+  setData(data);
+
   return {};
 }
 
@@ -87,6 +130,6 @@ function adminQuizNameUpdate(authUserId, quizId, name) {
  * 
  * @return {object} - Returns an empty object
  */
-function adminQuizDescriptionUpdate(authUserId, quizId, description) {
+export function adminQuizDescriptionUpdate(authUserId, quizId, description) {
   return {};
 }
