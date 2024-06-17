9c84a51c99b82a6e088b9e425542d14612bd1d25
Krish Kadamb Patel <z5508882@ad.unsw.edu.au>
2024-06-17 16:41:34 +1000

    Added the function adminQuizInfo

--- src/quiz.js
+++ src/quiz.js
@@ -55,15 +55,46 @@ function adminQuizRemove(authUserId, quizId) {
  * 
  * @return {object} - Returns an empty object
  */
+
 function adminQuizInfo(authUserId, quizId) {
-  return {
-    quizId: 1,
-    name: 'My Quiz',
-    timeCreated: 1683125870,
-    timeLastEdited: 1683125871,
-    description: 'This is my quiz',
-  };
-}
+    // Validate user ID
+    let user = null;
+    for (let i = 0; i < data.users.length; i++) {
+      if (data.users[i].uId === authUserId) {
+        user = data.users[i];
+        break;
+      }
+    }
+    if (!user) {
+      return { error: 'User ID is not valid' };
+    }
+  
+    // Validate quiz ID
+    let quiz = null;
+    for (let i = 0; i < data.quizzes.length; i++) {
+      if (data.quizzes[i].quizId === quizId) {
+        quiz = data.quizzes[i];
+        break;
+      }
+    }
+    if (!quiz) {
+      return { error: 'Quiz ID does not refer to a valid quiz' };
+    }
+  
+    // Check ownership
+    if (quiz.ownerId !== authUserId) {
+      return { error: 'Quiz ID does not refer to a quiz that this user owns' };
+    }
+  
+    // Return quiz details
+    return {
+      quizId: quiz.quizId,
+      name: quiz.name,
+      timeCreated: quiz.timeCreated,
+      timeLastEdited: quiz.timeLastEdited,
+      description: quiz.description,
+    };
+  }
 
 /**
  * This function updates the name of the relevant quiz.
