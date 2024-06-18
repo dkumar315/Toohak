908c133d8311b4e578d8ef2138f5527e94b14fc1
Akshat Mishra <z5461265@ad.unsw.edu.au>
2024-06-18 15:18:44 +1000

    adding final testfile for adminquizlist

--- src/adminQuiz.test.js
+++ src/adminQuiz.test.js
@@ -18,27 +18,27 @@ beforeEach(() => {
 
 test('adminQuizList returns error when authUserId is not valid', () => {
     const result = adminQuizList(789);
-    expect(result).toEqual({ error: 'AuthUserId is not valid' });
+    expect(result).toStrictEqual({ error: 'AuthUserId is not valid' });
   });
   
   test('adminQuizList returns an empty list for a user with no quizzes', () => {
     adminAuthRegister('akshatmishra@gmail.com', 'aks123456', 'Akshat', 'Mishra');
     const user = adminAuthLogin('akshatmishra@gmail.com', 'aks123456');
     const authUserId = user.authUserId;
-    const result = adminQuizList(authUserId); // Add this line to define result
-    console.log(user);
-    expect(result).toEqual({"quizzes": []});
+    const result = adminQuizList(authUserId);
+    expect(result).toStrictEqual({"quizzes": []});
   });
   
   test('adminQuizList returns a list of quizzes for the logged-in user', () => {
-    const user = adminAuthRegister('tonystark@gmail.com', 'assemble', 'Tony', 'Stark');
+    const user = adminAuthRegister('tonystark@gmail.com', 'assem1234ble', 'Tony', 'Stark');
     const authUserId = user.authUserId;
-    const quiz1 = adminQuizCreate(authUserId, 'Quiz 1', 'Description 1');
-    const quiz2 = adminQuizCreate(authUserId, 'Quiz 2', 'Description 2');
-    
+    const quiz1Name = 'Quiz 1';
+    const quiz2Name = 'Quiz 2';
+    const quiz1 = adminQuizCreate(authUserId, quiz1Name, 'Description 1');
+    const quiz2 = adminQuizCreate(authUserId, quiz2Name, 'Description 2');
     const result = adminQuizList(authUserId);
-    expect(result.quizzes).toEqual([
-      { quizId: quiz1.id, name: quiz1.name },
-      { quizId: quiz2.id, name: quiz2.name }
+    expect(result.quizzes).toStrictEqual([
+      { quizId: quiz1.quizId, name: quiz1Name },
+      { quizId: quiz2.quizId, name: quiz2Name }
     ]);
   });
