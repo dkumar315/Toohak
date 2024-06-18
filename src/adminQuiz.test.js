55e7c214b59575172f5c65a96e1b8d5f859174aa
Krish Kadamb Patel <z5508882@ad.unsw.edu.au>
2024-06-18 14:53:04 +1000

    Used toStrictEqual, changed a created quiz's owner and fixed 2 test cases

--- src/adminQuiz.test.js
+++ src/adminQuiz.test.js
@@ -18,55 +18,56 @@ describe('adminQuizInfo tests', () => {
   beforeEach(() => {
   clear();
   userId1 = adminAuthRegister('krishpatel@gmail.com', 'KrishP', 'Krish', 'Patel');
-  userId2 = adminAuthRegister('joshhoward@gmail.com', 'JoshH', 'Josh', 'Howard');
   adminAuthLogin('krishpatel@gmail.com', 'KrishP');
   quizId1 = adminQuizCreate(userId1.authUserId, 'My Quiz', 'Quiz on Testing');
-  quizId2 = adminQuizCreate(userId1.authUserId, 'Second Quiz', 'Another quiz for testing');
+  userId2 = adminAuthRegister('joshhoward@gmail.com', 'JoshH', 'Josh', 'Howard');
+  adminAuthLogin('joshhoward@gmail.com', 'JoshH');
+  quizId2 = adminQuizCreate(userId2.authUserId, 'Second Quiz', 'Another quiz for testing');
   });
 
   test('Successfully retrieves quiz information', () => {
     const result = adminQuizInfo(userId1.authUserId, quizId1.quizId);
-    expect(result).toEqual({
+    expect(result).toStrictEqual({
       quizId: quizId1.quizId,
-      name: 'My Quiz',
+      name: quizId1.name,
       timeCreated: expect.any(Number),
       timeLastEdited: expect.any(Number),
-      description: 'Quiz on Testing',
+      description: quizId1.description,
     });
   });
 
   test('Error shown when user ID is invalid', () => {
     const result = adminQuizInfo(999, quizId1.quizId);
-    expect(result).toEqual({ error: 'User ID is not valid' });
+    expect(result).toStrictEqual({ error: 'User ID is not valid' });
   });
 
   test('Error shown when quiz ID is invalid', () => {
     const result = adminQuizInfo(userId1.authUserId, 999);
-    expect(result).toEqual({ error: 'Quiz ID does not refer to a valid quiz' });
+    expect(result).toStrictEqual({ error: 'Quiz ID does not refer to a valid quiz' });
   });
 
   test('Error shown when user does not own the quiz', () => {
-    const result = adminQuizInfo(userId2.authUserId, quizId1.quizId);
-    expect(result).toEqual({ error: 'Quiz ID does not refer to a quiz that this user owns' });
-  });
-
-  test('Error shown when quiz ID is a string instead of an integer', () => {
-    const result = adminQuizInfo(userId1.authUserId, 'invalidQuizId');
-    expect(result).toEqual({ error: 'Quiz ID does not refer to a valid quiz' });
+    const result = adminQuizInfo(userId2.authUserId, quizId2.quizId);
+    expect(result).toStrictEqual({ error: 'Quiz ID does not refer to a quiz that this user owns' });
   });
 
   test('Error shown when user ID is a string instead of an integer', () => {
     const result = adminQuizInfo('invalidUserId', quizId1.quizId);
-    expect(result).toEqual({ error: 'User ID is not valid' });
+    expect(result).toStrictEqual({ error: 'User ID is not valid' });
+  });
+
+  test('Error shown when quiz ID is a string instead of an integer', () => {
+    const result = adminQuizInfo(userId1.authUserId, 'invalidQuizId');
+    expect(result).toStrictEqual({ error: 'Quiz ID does not refer to a valid quiz' });
   });
 
   test('Error shown when quiz ID is null', () => {
     const result = adminQuizInfo(userId1.authUserId, null);
-    expect(result).toEqual({ error: 'Quiz ID does not refer to a valid quiz' });
+    expect(result).toStrictEqual({ error: 'Quiz ID does not refer to a valid quiz' });
   });
 
   test('Error shown when user ID is null', () => {
     const result = adminQuizInfo(null, quizId1.quizId);
-    expect(result).toEqual({ error: 'User ID is not valid' });
+    expect(result).toStrictEqual({ error: 'User ID is not valid' });
   });
 });
