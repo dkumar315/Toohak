f7ba79c46c6985715ca18dc214933ce563320ebe
Yuhua Zheng <z5411307@ad.unsw.edu.au>
2024-06-20 16:25:06 +1000

    change all double quotes to single quote

--- src/adminAuth.test.js
+++ src/adminAuth.test.js
@@ -10,7 +10,7 @@ let result;
 
 beforeEach(()=> {
   clear();
-  result = adminAuthRegister("jane@example.com", "MyPassw0rd","Jane", "Smith");
+  result = adminAuthRegister('jane@example.com', 'MyPassw0rd', 'Jane', 'Smith');
 });
 
 describe('adminAuthRegister', () => {
@@ -20,68 +20,68 @@ describe('adminAuthRegister', () => {
     });
 
     test('test for names conatins spaces, hyphens, and apostrophes', () => {
-      const name = "J a-n\'e";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", name, "Smith");
+      const name = 'J a-n\'e';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', name, 'Smith');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
 
-      result = adminAuthRegister("test2@example.com", "MyPassw0rd", "jane", name);
+      result = adminAuthRegister('test2@example.com', 'MyPassw0rd', 'jane', name);
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
     });
 
     test('Test for names just meet require (name length = 2)', () => {
-      const name = "Ja";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", name, "Smith");
+      const name = 'Ja';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', name, 'Smith');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
 
-      result = adminAuthRegister("test2@example.com", "MyPassw0rd", "Jane", name);
+      result = adminAuthRegister('test2@example.com', 'MyPassw0rd', 'Jane', name);
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
     });
 
     test('Test for names just meet require (name length = 20)', () => {
-      const name = "JJJJJJJJJJJJJJJJJJJJ";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", name, "Smith");
+      const name = 'JJJJJJJJJJJJJJJJJJJJ';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', name, 'Smith');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
 
-      result = adminAuthRegister("test2@example.com", "MyPassw0rd", "Jane", name);
+      result = adminAuthRegister('test2@example.com', 'MyPassw0rd', 'Jane', name);
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
     });
 
     test('Test for password just meet require (length = 8)', () => {
-      const password = "passw0rd";
-      result = adminAuthRegister("test@example.com", password, "Jane", "Smith");
+      const password = 'passw0rd';
+      result = adminAuthRegister('test@example.com', password, 'Jane', 'Smith');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
     });
 
     test('Test for password with specail characters', () => {
-      const password = "passw0rd@#&/?><|";
-      result = adminAuthRegister("test@example.com", password, "Jane", "Smith");
+      const password = 'passw0rd@#&/?><|';
+      result = adminAuthRegister('test@example.com', password, 'Jane', 'Smith');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
     });
 
     test('Test for mutiple users', () => {
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", "Jane", "Smith");
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', 'Jane', 'Smith');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
 
-      result = adminAuthRegister("test2@example.com", "MyPassw0rd", "Jane", "Smith");
+      result = adminAuthRegister('test2@example.com', 'MyPassw0rd', 'Jane', 'Smith');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
     });
   })
 
   describe('valid Email creation', () => {
     test('Email address is used by another user', () => {
-      const email = "test@example.com";
-      const result1 = adminAuthRegister(email, "MyPassw0rd", "Jane", "Smith");
+      const email = 'test@example.com';
+      const result1 = adminAuthRegister(email, 'MyPassw0rd', 'Jane', 'Smith');
       expect(result1).toStrictEqual({ authUserId: expect.any(Number) });
 
-      result = adminAuthRegister("jane@example.com", "MyPassw0rd", "Sarah", "Parker");
+      result = adminAuthRegister('jane@example.com', 'MyPassw0rd', 'Sarah', 'Parker');
       expect(result).toStrictEqual({ error: 'Invalid email.' });
       
-      const result2 = adminAuthRegister(email, "MyPassw0rd", "Sarah", "Parker");
+      const result2 = adminAuthRegister(email, 'MyPassw0rd', 'Sarah', 'Parker');
       expect(result2).toStrictEqual({ error: 'Invalid email.' });
     });
   
     test('Email does not satisfy (email with string only)', () => {
-      result = adminAuthRegister("123", "MyPassw0rd", "Jane", "Smith");
+      result = adminAuthRegister('123', 'MyPassw0rd', 'Jane', 'Smith');
       expect(result).toStrictEqual({ error: 'Invalid email.' });
     });
   });
@@ -90,51 +90,51 @@ describe('adminAuthRegister', () => {
     const nameFirstError = 'Firstname does not meet requirements.';
     // NameFirst
     test('Test for invalid nameFirst input (number)', () => {
-      const nameFirst = "Jan3";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", nameFirst, "Smith");
+      const nameFirst = 'Jan3';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', nameFirst, 'Smith');
       expect(result).toStrictEqual({ error: nameFirstError });
     });
 
     test('Test for invalid nameFirst input (@#&/?><|)', () => {
-      const nameFirst = "J@#&/?><|ne";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", nameFirst, "Smith");
+      const nameFirst = 'J@#&/?><|ne';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', nameFirst, 'Smith');
       expect(result).toStrictEqual({ error: nameFirstError });
     });  
 
     test('Test for nameFirst length less than 2 characters', () => {
-      const nameFirst = "J";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", nameFirst, "Smith");
+      const nameFirst = 'J';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', nameFirst, 'Smith');
       expect(result).toStrictEqual({ error: nameFirstError });
     });
 
     test('Test for nameFirst length exceeding 20 characters', () => {
-      const nameFirst = "JaneJaneJaneJaneJanee";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", nameFirst, "Smith");
+      const nameFirst = 'JaneJaneJaneJaneJanee';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', nameFirst, 'Smith');
       expect(result).toStrictEqual({ error: nameFirstError });
     });        
 
     // nameLast
     const nameLastError = 'Lastname does not meet requirements.';
     test('Test for invalid nameLast input (number)', () => {
-      const result = adminAuthRegister("test@example.com", "MyPassw0rd","Jane", "5mith");
+      const result = adminAuthRegister('test@example.com', 'MyPassw0rd','Jane', '5mith');
       expect(result).toStrictEqual({ error: nameLastError });
     });
 
     test('Test for invalid nameLast input (@@#&/?><|)', () => {
-      const nameLast = "Sm@#&/?><|th";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", "Jane", nameLast);
+      const nameLast = 'Sm@#&/?><|th';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', 'Jane', nameLast);
       expect(result).toStrictEqual({ error: nameLastError });
     });  
 
     test('Test for nameLast length less than 2 characters', () => {
-      const nameLast = "S";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", "Jane", nameLast);
+      const nameLast = 'S';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', 'Jane', nameLast);
       expect(result).toStrictEqual({ error: nameLastError });
     });  
 
     test('Test for nameLast length exceeding 20 characters', () => {
-      const nameLast = "SmithSmithSmithSmiths";
-      result = adminAuthRegister("test@example.com", "MyPassw0rd", "Jane", nameLast);
+      const nameLast = 'SmithSmithSmithSmiths';
+      result = adminAuthRegister('test@example.com', 'MyPassw0rd', 'Jane', nameLast);
       expect(result).toStrictEqual({ error: nameLastError });
     });  
   });
@@ -142,20 +142,20 @@ describe('adminAuthRegister', () => {
   describe('Password creation - adminAuthRegister function', () => {
     const passwordError = 'Password does not meet requirements.';
     test('Test for password length less than 8 characters', () => {
-      const password = "Pass1";
-      result = adminAuthRegister("test@example.com", password, "Jane", "Smith");
+      const password = 'Pass1';
+      result = adminAuthRegister('test@example.com', password, 'Jane', 'Smith');
       expect(result).toStrictEqual({ error: passwordError });    
     });
 
     test('Test for password missing a number', () => {
-      const password = "MyPassword";
-      result = adminAuthRegister("test@example.com", password, "Jane", "Smith");
+      const password = 'MyPassword';
+      result = adminAuthRegister('test@example.com', password, 'Jane', 'Smith');
       expect(result).toStrictEqual({ error: passwordError });    
     });
 
     test('Test for password missing a letter', () => {
-      const password = "123456789";
-      const result = adminAuthRegister("test@example.com", "123456789", "Jane", "Smith");
+      const password = '123456789';
+      const result = adminAuthRegister('test@example.com', '123456789', 'Jane', 'Smith');
       expect(result).toStrictEqual({ error: passwordError });    
     });
   });
@@ -165,48 +165,48 @@ describe('adminAuthRegister', () => {
 describe('adminAuthLogin', () => {
   describe('valid input', () => {
     test('Test for mutiple logins', () => {
-      adminAuthLogin("jane@example.com", "MyPassw0rd");
-      adminAuthLogin("jane@example.com", "MyPassw0rd");
-      result = adminAuthLogin("jane@example.com", "MyPassw0rd");
+      adminAuthLogin('jane@example.com', 'MyPassw0rd');
+      adminAuthLogin('jane@example.com', 'MyPassw0rd');
+      result = adminAuthLogin('jane@example.com', 'MyPassw0rd');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
     });
 
     test('Test for mutiple users', () => {
-      adminAuthRegister("test@example.com", "passw0rd", "Jane", "Smith");
-      adminAuthRegister("testss@example.com", "passw0rd", "Jane", "Smith");
-      adminAuthRegister("tests@example.com", "passw0rd", "Jane", "Smith");
+      adminAuthRegister('test@example.com', 'passw0rd', 'Jane', 'Smith');
+      adminAuthRegister('testss@example.com', 'passw0rd', 'Jane', 'Smith');
+      adminAuthRegister('tests@example.com', 'passw0rd', 'Jane', 'Smith');
       
-      const result1 = adminAuthLogin("test@example.com", "passw0rd");
+      const result1 = adminAuthLogin('test@example.com', 'passw0rd');
       expect(result1).toStrictEqual({ authUserId: expect.any(Number) });
 
-      const result2 = adminAuthLogin("testss@example.com", "passw0rd");
+      const result2 = adminAuthLogin('testss@example.com', 'passw0rd');
       expect(result2).toStrictEqual({ authUserId: expect.any(Number) });
 
-      const result3 = adminAuthLogin("tests@example.com", "passw0rd");
+      const result3 = adminAuthLogin('tests@example.com', 'passw0rd');
       expect(result3).toStrictEqual({ authUserId: expect.any(Number) });
     });
   });
 
   describe('Email input', () => {
     test('Test for non-existent email', () => {
-      const result = adminAuthLogin(" ", "MyPassw0rd");
+      const result = adminAuthLogin(' ', 'MyPassw0rd');
       expect(result).toStrictEqual({ error: 'Invalid email.' });
     });
 
     test('Test for wrong email', () => {
-      const result = adminAuthLogin("fakejane@example.com", "MyPassw0rd");
+      const result = adminAuthLogin('fakejane@example.com', 'MyPassw0rd');
       expect(result).toStrictEqual({ error: 'Invalid email.' });
     });
   });
 
   describe('Password input', () => { 
     test('Test for invalid password', () => {
-      const result = adminAuthLogin("jane@example.com", "Invalid Password");
+      const result = adminAuthLogin('jane@example.com', 'Invalid Password');
       expect(result).toStrictEqual({ error: 'Incorrect Password.' });
     });
 
     test('Test for empty password input', () => {
-      const result = adminAuthLogin("jane@example.com", "")
+      const result = adminAuthLogin('jane@example.com', '')
       expect(result).toStrictEqual({ error: 'Incorrect Password.' });
     });
   });
@@ -221,7 +221,7 @@ describe('adminAuthLogin', () => {
 
     test('Test for login with invalid email', () => {
       const authUserId = result.authUserId;
-      result = adminAuthLogin("invalid@example.com", "MyPassw0rd");
+      result = adminAuthLogin('invalid@example.com', 'MyPassw0rd');
       expect(result).toStrictEqual({ error: 'Invalid email.' });
 
       const userDetail = adminUserDetails(authUserId).user;
@@ -231,7 +231,7 @@ describe('adminAuthLogin', () => {
 
     test('Test for login with invalid password', () => {
       const authUserId = result.authUserId;
-      result = adminAuthLogin("jane@example.com", "Passw0rd");
+      result = adminAuthLogin('jane@example.com', 'Passw0rd');
       expect(result).toStrictEqual({ error: 'Incorrect Password.' });
 
       const userDetail = adminUserDetails(authUserId).user;
@@ -240,15 +240,15 @@ describe('adminAuthLogin', () => {
     });
 
     test('Test successful login after invalid password', () => {
-      adminAuthLogin("jane@example.com", "Passw0rd");
-      result = adminAuthLogin("jane@example.com", "MyPassw0rd");
+      adminAuthLogin('jane@example.com', 'Passw0rd');
+      result = adminAuthLogin('jane@example.com', 'MyPassw0rd');
       expect(result).toStrictEqual({ authUserId: expect.any(Number) });
 
       let userDetail = adminUserDetails(result.authUserId).user;
       expect(userDetail.numSuccessfulLogins).toStrictEqual(1);
       expect(userDetail.numFailedPasswordsSinceLastLogin).toStrictEqual(0);
 
-      result = adminAuthLogin("jane@example.com", "MyPassw0rd");
+      result = adminAuthLogin('jane@example.com', 'MyPassw0rd');
       userDetail = adminUserDetails(result.authUserId).user;
       expect(userDetail.numSuccessfulLogins).toStrictEqual(2);
       expect(userDetail.numFailedPasswordsSinceLastLogin).toStrictEqual(0);
