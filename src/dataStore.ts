a24b961f13a94bca7b6faba22025978db986fa85
Yuhua Zheng <z5411307@ad.unsw.edu.au>
2024-06-27 22:23:03 +1000

    Add change file to .ts and add https constant, user interface

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -4,6 +4,31 @@ let data = {
   quizzes: [],
 };
 
+// define constants
+const OK = 200;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const FORBIDDEN = 403;
+export { OK, BAD_REQUEST, UNAUTHORIZED, FORBIDDEN };
+
+export interface Data {
+  users: User[],
+  quizzes: any, // fixme
+}
+
+export interface User {
+  userId: number,
+  password: string,
+  email: string,
+  nameFirst: string,
+  nameLast: string,
+  numSuccessfulLogins: number,
+  numFailedPasswordsSinceLastLogin: number,
+  passwordHistory?: string[]
+}
+
+export type EmptyObject = Record<string, never>;
+
 // YOU SHOULD MODIFY THIS OBJECT ABOVE ONLY
 
 // YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
