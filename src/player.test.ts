e355cbe6d41d9dd612c75d20b436ec4cf6918aa3
Yuhua Zheng <z5411307@ad.unsw.edu.au>
2024-07-23 14:14:04 +1000

    player tests complete

--- src/player.test.ts
+++ src/player.test.ts
@@ -19,6 +19,8 @@ let token: string, quizId: number, questionId: number, sessionId: number;
 const autoStartNum: number = SessionLimits.AUTO_START_NUM_MAX - 1;
 let result: ResPlayerId | ResError;
 
+const validPlayerAdd: PlayerId = { palyerId: expect.any(Number) };
+
 beforeEach(() => {
   requestClear();
   token = authRegister('e@gmail.com', 'Passw0rd', 'nameFirst', 'nameLast').token;
@@ -47,36 +49,59 @@ afterAll(requestClear);
 describe('testing playerJoin POST /v1/player/join', () => {
   test('routes', () => {
     result = requestPlayerJoin(sessionId, 'name');
-    console.log(result);
     expect(typeof result === 'object' && 'playerId' in result &&
       typeof result.playerId === 'number').toBe(true);
   });
   describe('test 1.0 valid inputs', () => {
-    test('test 1.1 sessionId exist, player name empty', () => {
+    test('test 1.1 sessionId exist, playerNames empty', () => {
+      result = requestPlayerJoin(sessionId, '');
+      expect(result).toMatchObject(validPlayerAdd);
+      expect(result.status).toStrictEqual(OK);
 
+      result = requestPlayerJoin(sessionId, '');
+      expect(result).toMatchObject(validPlayerAdd);
+      expect(result.status).toStrictEqual(OK);
     });
     test('test 1.2 sessionId exist, quiz in trash / deleted', () => {
+      requestQuizRemove(token, quizId);
+      result = requestPlayerJoin(sessionId, '');
+      expect(result).toMatchObject(validPlayerAdd);
+      expect(result.status).toStrictEqual(OK);
 
+      requestQuizEmptyTrash(token, [quizId]);
+      result = requestPlayerJoin(sessionId, '');
+      expect(result).toMatchObject(validPlayerAdd);
+      expect(result.status).toStrictEqual(OK);
     });
     test('test 1.3 sessionId exist, player name not empty', () => {
-
+      result = requestPlayerJoin(sessionId, 'name');
+      expect(result).toMatchObject(validPlayerAdd);
+      expect(result.status).toStrictEqual(OK);
     });
   });
 
   describe('test 2.0 invalid inputs', () => {
     test('test 2.1 sessionId not exist / incorrect format', () => {
-
+      result = requestPlayerJoin(sessionId + 1, 'name');
+      expect(result).toMatchObject(ERROR);
+      expect(result.status).toStrictEqual(BAD_REQUEST);
     });
-    test('test 2.2 sessionId refer to session with other than LOBBY state', () => {
-
+    test.skip('test 2.2 sessionId refer to session with other than LOBBY state', () => {
+      // requires update session state
     });
     test('test 2.3 name of user is used in the session', () => {
+      result = requestPlayerJoin(sessionId, 'name');
+      expect(result).toMatchObject(validPlayerAdd);
+      expect(result.status).toStrictEqual(OK);
 
+      result = requestPlayerJoin(sessionId, 'name');
+      expect(result).toMatchObject(ERROR);
+      expect(result.status).toStrictEqual(BAD_REQUEST);
     });
   });
   describe('test 3.0 player data setup correctly', () => {
     test.skip('test 3.1 playerInfo: player initial state', () => {
-
+      // require player Info
     });
   });
 });
--- src/player.ts
+++ src/player.ts
@@ -6,5 +6,36 @@ import {
 export type PlayerId = { playerId: number };
 
 export function playerJoin(sessionId: number, name: string): PlayerId {
+  const sessionIndex: number = findSession(sessionId);
+  const session: QizSession = data.quizSessions[sessionIndex];
+
+  // let playerName = name || generateRandomName();
+  if (name.length === 0) {
+    const name: string === generateName();
+  } else if (isExistName(name)) {
+    throw new Error(`Invalid ${name}, already exists in the session.`)
+  }
+
+  setPlayer(name);
+
   return { playerId: 0 };
 }
+
+function generateRandomName() {
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numbers = '0123456789';
+  
+  const randomLetters = [...letters]
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 5)
+    .join('');
+  
+  const randomNumbers = [...numbers]
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 3)
+    .join('');
+  
+  return randomLetters + randomNumbers;
+}
+
+console.log(generateRandomName());
