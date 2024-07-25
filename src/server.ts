acfc56323a593326512b93c89bbd571f367953c5
Yuhua Zheng <z5411307@ad.unsw.edu.au>
2024-07-26 01:26:02 +1000

    v1 quizQuestion coverage done

--- src/server.ts
+++ src/server.ts
@@ -256,7 +256,7 @@ app.post('/v2/admin/quiz/:quizid/restore', (req: Request, res: Response) => {
 
 // Permanently delete specific quizzes currently sitting in the trash (v1)
 app.delete('/v1/admin/quiz/trash/empty', (req: Request, res: Response) => {
-  const token: string = req.query.token;
+  const token: string = req.query.token as string;
   const quizIds: number[] = (req.query.quizIds as string[]).map(Number);
   return res.json(adminQuizTrashEmpty(token, quizIds));
 });
